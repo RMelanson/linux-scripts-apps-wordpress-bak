cfd30043dd323ac161387c7ee444882f4abdc1af
Robin Melanson <remcogold@gmail.com>
2019-03-14 21:36:25 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # WP BACKUP CONFIGURATION PARAMETERS
+pkg=WP_BAK
 gitRepo="linux-scripts-apps-wp-backuprestore.git"
-installDir="/tmp/apps/backuprestore"
-pkg=wpbak
+installDir="/tmp/utils/$pkg"
